286ba362dcab7eb15ebe8f95e0f4420669a6e626
Arthur Jen <cowboy5110@hotmail.com>
2014-08-19 01:13:39 -0700

    Add new custom message

--- lib/status.py
+++ lib/status.py
@@ -4,7 +4,17 @@
 class ResourceNotFound(Exception):
     def __init__(self, resourceType, **kwargs):
         errMessage = {
-            'message': 'Resource Not Found',
+            'statusMessage': 'Resource Not Found',
+            'resourceType': resourceType
+        }
+        for k, v in kwargs.iteritems():
+            errMessage[k] = v
+        self.errors = errMessage
+
+class InvalidRequest(Exception):
+    def __init__(self, resourceType, **kwargs):
+        errMessage = {
+            'statusMessage': 'Invalid Request',
             'resourceType': resourceType
         }
         for k, v in kwargs.iteritems():
