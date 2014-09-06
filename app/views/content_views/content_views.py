09bf0000a6f71f5545f4ff286c18e5ddf737ee10
Arthur Jen <cowboy5110@hotmail.com>
2014-09-06 13:35:09 -0400

    Update projects and resume content view endpoints

--- app/views/content_views/content_views.py
+++ app/views/content_views/content_views.py
@@ -1,20 +1,72 @@
-from flask import render_template
+from collections import OrderedDict
+from flask import render_template, abort
+import json
+import requests
 
-from app import ajen_webSite
+from app import ajen_webSite, ajen_api
 from app.config import default_config as setting
+
+HOST = ajen_api.config['SERVER_NAME']
+QUERY = 'http://%s/api/%s/%s'
 
 @ajen_webSite.route('/', methods=['GET'])
 @ajen_webSite.route('/about/', methods=['GET'])
 def about():
     return render_template('about.html', headers=setting.BASE_PAGE)
 
+@ajen_webSite.route('/resume/<string:displayType>/')
 @ajen_webSite.route('/resume/', methods=['GET'])
-def resume():
-    return render_template('resume.html', headers=setting.BASE_PAGE)
+def resume(displayType='html'):
+    try:
+        params = {
+            'deref': ['address', 'skills', 'workPlaces', 'projects', 'schools']
+        }
+        resume = requests.get(QUERY % (HOST, 'user', 1), params=params)
+    except Exception:
+        abort(500)
+
+    # Even requests library comes with json function, we will with the string
+    # content to it to json specifically because the order is important to us.
+    # Hooking up json.loads() with OrderedDict to achieve that functionality.
+    resume = json.loads(resume.content, object_pairs_hook=OrderedDict)
+
+    if resume['status']['statusCode'] == 'HTTPOk' and resume['result']:
+        displayType = displayType.lower()
+
+        # Check which formate we should use: either JSON or HTML. If it is JSON
+        # we will dump the JSON using json.dumps, else we will just return the
+        # resume variable.
+        if displayType == 'json':
+            resume = json.dumps(resume['result'])
+        elif displayType == 'html':
+            resume = resume['result']
+        else:
+            abort(404)
+
+        return render_template('resume.html', headers=setting.BASE_PAGE,
+                                resume=resume, displayType=displayType)
+
+    elif resume['status']['statusCode'] == 'InternalServerError':
+        abort(500)
+    else:
+        abort(404)
 
 @ajen_webSite.route('/projects/', methods=['GET'])
 def projects():
-    return render_template('projects.html', headers=setting.BASE_PAGE)
+    try:
+        projects = requests.get(QUERY % (HOST, 'project', ''),
+                                params={'deref': 'tasks'})
+    except Exception:
+        abort(500)
+
+    projects = projects.json()
+    if projects['status']['statusCode'] == 'HTTPOk' and projects['result']:
+        return render_template('projects.html', headers=setting.BASE_PAGE,
+                               projects=projects['result'])
+    elif projects['status']['statusCode'] == 'InternalServerError':
+        abort(500)
+    else:
+        abort(404)
 
 @ajen_webSite.route('/contact/', methods=['GET'])
 def contact():
